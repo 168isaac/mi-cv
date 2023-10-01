ea486835170900b7d327b65f9177098d65e29adf
androman168 <66146597+androman168@users.noreply.github.com>
2023-09-30 22:03:28 -0600

    update

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({ pageLanguage: 'es', includedLanguages: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true }, 'google_translate_element');
+}
